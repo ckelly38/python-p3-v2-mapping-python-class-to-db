4f587c0b34bc6eb6bc707d330b1e48fc3ff35e20
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-11-04 07:40:37 -0600

    this is the ORM code along for Python. It shows how Python and SQL interact by using Objects as wrappers to temporarily hold database information which can be very useful

--- lib/department.py
+++ lib/department.py
@@ -9,4 +9,41 @@ class Department:
         self.location = location
 
     def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+        return f"<Department {self.id}: {self.name}, {self.location}>";
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute(
+            "CREATE TABLE IF NOT EXISTS departments (id INTEGER PRIMARY KEY, name TEXT, location TEXT);");
+        CONN.commit();
+        return None;
+
+    @classmethod
+    def create(cls, name, location):
+        department = cls(name, location);
+        department.save();
+        return department;
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute("DROP TABLE IF EXISTS departments;");
+        CONN.commit();
+        return None;
+
+    def save(self):
+        CURSOR.execute("INSERT INTO departments (name, location) VALUES (?, ?)",
+                       (self.name, self.location));
+        CONN.commit();
+        self.id = CURSOR.lastrowid;
+        return None;
+
+    def update(self):
+        CURSOR.execute("UPDATE departments SET name = ?, location = ? WHERE id = ?",
+                       (self.name, self.location, self.id));
+        CONN.commit();
+        return None;
+
+    def delete(self):
+        CURSOR.execute("DELETE FROM departments WHERE id = ?", (self.id,));
+        CONN.commit();
+        return None;
